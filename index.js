18decfafeae263ec0e78979609cba118c97bb73b
viniciusgcardozo <vinicius.gcardozo@gmail.com>
2018-01-26 21:54:42 -0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static('.')); //Diretorio base para a aplicação
+
+app.listen(port);
+console.log("App listening on port " + port);
